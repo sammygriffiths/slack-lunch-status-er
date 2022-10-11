9ee45a57f9fbc4da28377a75df5ead36eadf3bf8
Sammy Griffiths <sammy@sammygriffiths.co.uk>
2022-10-11 15:45:06 +0100

    Remove left over debug code

--- index.js
+++ index.js
@@ -11,19 +11,13 @@ const run = async () => {
     const status      = statuses[Math.floor(Math.random() * statuses.length)];
     const statusEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-    console.log({
-        status_text: status,
-        status_emoji: `:${statusEmoji}:`,
-        status_expiration: (Date.now() / 1000) + (config.expireIn * 60)
-    });
-
-    // client.users.profile.set({
-    //     profile: {
-    //         status_text: status,
-    //         status_emoji: `:${statusEmoji}:`,
-    //         status_expiration: (Date.now() / 1000) + (config.expireIn * 60)
-    //     }
-    // }).catch(console.error);
+    client.users.profile.set({
+        profile: {
+            status_text: status,
+            status_emoji: `:${statusEmoji}:`,
+            status_expiration: (Date.now() / 1000) + (config.expireIn * 60)
+        }
+    }).catch(console.error);
 }
 
 run();
